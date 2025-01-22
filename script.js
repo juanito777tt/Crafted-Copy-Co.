739b9faac9d1553cf6f4cac0a032cbe6c1bff608
juanito777tt <149965418+juanito777tt@users.noreply.github.com>
2025-01-22 15:47:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    alert('Thank you for reaching out! We will get back to you shortly.');
-});
 
